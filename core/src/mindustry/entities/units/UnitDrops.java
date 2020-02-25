6047850066bea06ac50bc1288c2f5d731e622469
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-02-25 03:56:54 +0100

    unit drops

--- core/src/mindustry/entities/units/UnitDrops.java
+++ core/src/mindustry/entities/units/UnitDrops.java
@@ -7,36 +7,38 @@ import mindustry.entities.type.BaseUnit;
 import mindustry.entities.type.TileEntity;
 import mindustry.gen.Call;
 import mindustry.type.Item;
+
+import java.util.HashMap;
+
 import static mindustry.Vars.*;
 
 public class UnitDrops{
-    private static Item[] dropTable;
+    private static HashMap<Item, Float> drops = new HashMap<>(); // item, dropchance
 
     public static void dropItems(BaseUnit unit){
         //items only dropped in waves for enemy team
-        if(unit.getTeam() != state.rules.waveTeam || !Vars.state.rules.unitDrops){
+        if(unit.getTeam() != state.rules.waveTeam){
             return;
         }
+
+        drops.put(Items.copper, 0.5f);
+        drops.put(Items.lead, 0.5f);
+        drops.put(Items.graphite, 0.3f);
+        drops.put(Items.silicon, 0.25f);
+        drops.put(Items.titanium, 0.15f);
+        drops.put(Items.metaglass, 0.1f);
+        drops.put(Items.thorium, 0.05f);
+        drops.put(Items.plastanium, 0.02f);
+        drops.put(Items.surgealloy, 0.01f);
+        drops.put(Items.phasefabric, 0.005f);
 
         TileEntity core = unit.getClosestEnemyCore();
 
-        if(core == null || core.dst(unit) > Vars.mineTransferRange){
-            return;
-        }
-
-        if(dropTable == null){
-            dropTable = new Item[]{Items.titanium, Items.silicon, Items.lead, Items.copper};
-        }
 
         for(int i = 0; i < 3; i++){
-            for(Item item : dropTable){
-                //only drop unlocked items
-                if(!Vars.headless && !Vars.data.isUnlocked(item)){
-                    continue;
-                }
-
+            for(Item item : drops){ // TODO: make this entryset loop, use chance & item.
                 if(Mathf.chance(0.03)){
-                    int amount = Mathf.random(20, 40);
+                    int amount = Mathf.random(1, 40);
                     amount = core.tile.block().acceptStack(item, amount, core.tile, null);
                     if(amount > 0){
                         Call.transferItemTo(item, amount, unit.x + Mathf.range(2f), unit.y + Mathf.range(2f), core.tile);
